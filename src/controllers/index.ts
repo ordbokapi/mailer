8461e5704f02442a42e6fa07b96fe20b6483eda2
Adaline Simonian <adalinesimonian@gmail.com>
2024-02-22 16:57:27 -0800

    feat: legg til notify-kontroller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 export * from './app.controller';
+export * from './notify.controller';
 export * from './preview.controller';
 export * from './subscription.controller';
 export * from './test.controller';
